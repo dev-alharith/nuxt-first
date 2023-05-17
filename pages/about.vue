57c1727285e7bb4196e0f2c201cfb86294b2c746
Alharith Ali <work.alharith@gmail.com>
2023-05-17 23:04:29 +0300

    third commit

--- pages/about.vue
+++ pages/about.vue
@@ -1,17 +1,47 @@
 <script setup>
-    const {data} = await useFetch('/api/currency/USD');
+import sourceData from '/currencies.json';
+
+const currrencyCode = ref({
+    code:''
+});
+const result = reactive({
+    code:null
+});
+
+
+watch(currrencyCode,async ()=>{
+        const  response  = await fetch(`/api/currency/${currrencyCode.value}`)
+        const data = await response.json()
+        result.code = data;
+})
 </script>
 
 <template>
     <div>
         <h1>About</h1>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id ratione dolor at voluptas consectetur culpa dolorem quos voluptatem minima repellendus?</p>
-        <div>
-            {{ data }}
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id ratione dolor at voluptas consectetur culpa dolorem
+            quos voluptatem minima repellendus?</p>
+        <div class="container my-7 py-7">
+            <select v-model="currrencyCode" class="p-4 rounded">
+                <option value="" disabled selected>Please Select a Currency</option>
+                <option :value="currency.code" v-for="currency in sourceData">
+                    {{ currency.code }} - {{ currency.name }}
+                </option>
+            </select>
+        </div>
+        <div v-if="result.code">
+            <div>
+                1 USD is equal to {{ result.code[Object.getOwnPropertyNames(result.code)[0].toString()].value }}  {{ currrencyCode }}
+            </div>
+        </div>
+        <div class="grid grid-cols-4 gap-4 mt-7">
+            <div v-for="currency in sourceData" class="bg-white shadow-md p-4 rounded flex justify-between items-center">
+                <div class="">{{ currency.name + ' / ' + currency.code }}</div>
+                <div class="bg-green-500 rounded-lg w-14  text-center">{{ currency.symbol }}</div>
+            </div>
         </div>
     </div>
 </template>
 
 
-<style>
-</style>
+<style></style>
